6f97a926aafd4966578d1f34188f70ecde67c2de
brunogcsousa <bruno.g.c.sousa@gmail.com>
2023-04-06 21:35:29 -0300

    APP do Scooby doo doodod odod o o

---
+++ Scoobydoo2/app/src/main/java/com/example/scooby_doo/jogo.java
@@ -0,0 +1,14 @@
+package com.example.scooby_doo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class jogo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jogo);
+    }
+}
